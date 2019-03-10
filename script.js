e0b8579609f7a4257cea2ea655d318cffdbe81f5
Maksim Kunaev <maksim.kunaev@latoken.com>
2019-03-10 04:40:41 +0300

    v0.1

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
-// const data = [8.702728327689854, 2.544332165436225, 8.499468893258719, 3.1948647344875147, 8.658347530160848, 2.5003430825924955, 2.3728092066329998, 3.568301386645256, 7.438883602299978, 9.923708565660936, 7.407306422316426, 0.9565464957349334, 6.3378250966269185, 9.789070192977874, 4.679942351801907, 8.192381099471419, 0.9757396511331651, 5.162970188237351, 1.6626012747433827, 1.7497245445229415, 4.2376916583243425, 0.8839849666347677, 9.750875845820705, 6.544106345265604, 2.9503739887080793, 5.2938144445416295, 8.79028102858701, 6.633624803805162, 5.104750145614649, 6.1582931450273986, 3.660768581316478, 1.8172477864601544, 5.277156774733607, 9.435013942798378, 7.456377486000159, 4.754815356907103, 0.14523240236696822, 2.679347091352653, 5.17248006208177, 9.913050221426392];
-var data = new Array(50).fill(1);
+const data = new Array(50).fill(1);
 
 data.forEach((i, idx)=>{
     data[idx] = Math.random() * 20;
@@ -106,26 +104,27 @@ const right = previewWidth + 'px';
 lineChart.style.backgroundImage = `linear-gradient(90deg, #3c3c3c61 ${left}, #ffffff00 ${left}, #ffffff00 ${right},#3c3c3c61 ${right})`;
 
 preView.onmousedown = function(e) {
-    var coords = getCoords(preView);
-    var shiftX = e.pageX - coords.left;
-    var shiftXRight = coords.right - e.pageX;
+    const preViewCoords = getCoords(preView);
+    const shiftX = e.pageX - preViewCoords.left;
 
-    preView.style.position = 'absolute';
     moveAt(e);
 
     preView.style.zIndex = 1000; // над другими элементами
 
     function moveAt(e) {
-        // if (e.pageX - shiftX < wrapCoor.left) {
-        //     preView.style.left = '0px';
-        // } else if (e.pageX + shiftXRight + wrapCoor.left > wrapCoor.right) {
-        //     preView.style.right = wrapCoor.right + wrapCoor.left + 'px';
-        // } else {
-        //     preView.style.left = e.pageX - shiftX + 'px';
-        // }
-        preView.style.left = e.pageX - shiftX + 'px';
+        let newLeft = e.pageX - shiftX - wrapCoor.left;
+        if (newLeft < 0) {
+            newLeft = 0;
+        }
+
+        const rightEdge = lineChart.offsetWidth - preView.offsetWidth;
+        if (newLeft > rightEdge) {
+            newLeft = rightEdge;
+        }
+        preView.style.left = newLeft + 'px';
+
         const left = preView.style.left;
-        const right = e.pageX + shiftXRight + 'px';
+        const right = newLeft + preView.offsetWidth + 'px';
 
         lineChart.style.backgroundImage = `linear-gradient(90deg, #3c3c3c61 ${left}, #ffffff00 ${left}, #ffffff00 ${right},#3c3c3c61 ${right})`;
 
