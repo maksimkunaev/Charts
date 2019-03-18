87548358d3ce1c71b26904d2bfa42d03a7dec28a
Maksim Kunaev <maksim.kunaev@latoken.com>
2019-03-19 02:37:05 +0700

    v0.75

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const Chart = require('./charts');
 const Thumb = require('./thumb');
 
 const chart_data = require('./chart_data');
-const data = chart_data[3];
+const data = chart_data[1];
 
 const shortChart = new Chart('view', data);
 const longChart = new Chart('timeLine', data);
@@ -22,3 +22,29 @@ function drawCharts(data) {
 }
 
 drawCharts(data);
+
+const switcher = document.querySelector('.swither');
+const thumb = document.querySelector('.thumb');
+const swithLabel = document.querySelector('.swithLabel');
+const labelText = swithLabel.querySelector('span');
+switcher.addEventListener('change', onChange);
+
+function onChange({target}) {
+    const { checked } = target;
+    changeTheme(checked);
+}
+function changeTheme(checked) {
+    if (checked) {
+        document.body.style.backgroundColor = '#242f3e';
+        thumb.classList.toggle('thumb-nightTheme');
+        labelText.innerText = 'Switch to Day mode';
+        labelText.style.color = '#35a8f1'
+    } else {
+        document.body.style.backgroundColor = 'transparent';
+        thumb.classList.toggle('thumb-nightTheme');
+        labelText.innerText = 'Switch to Night mode'
+        labelText.style.color = '#000'
+    }
+}
+
+changeTheme(true);
