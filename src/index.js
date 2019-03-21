2b45bba947745a40b59e0b18a69bf6d6c275925c
Maksim Kunaev <maksim.kunaev@latoken.com>
2019-03-22 03:50:44 +0700

    v1.17

--- src/index.js
+++ src/index.js
@@ -34,17 +34,49 @@ class Init {
 
         this.createTemplate(id);
         this.initChart();
-        this.init(id, data);
         this.initSlider();
 
+        this.template.switcher.addEventListener('change', this.onChangeTheme);
+        this.switchTheme.call(this, 'day');
+
+        this.createCheckboxes();
+    }
+
+    createCheckboxes = () => {
+        const { shortChart, data } = this;
 
         const {
-            switcher,
+            checkboxes,
         } = this.template;
 
-        switcher.addEventListener('change', this.onChangeTheme);
-        this.switchTheme.call(this, 'day');
-    }
+        const { renderChart } = this;
+
+        function switchData(data) {
+            const changeData = (idx, color, e)=> {
+                const { checked } = e.target;
+                const parent = e.target.parentNode;
+                const customCheckbox = parent.querySelector('.custom-checkbox');
+
+                config[idx].isVisible = checked;
+                customCheckbox.style.backgroundColor = checked ? color : 'transparent';
+                shortChart.renderChart({
+                    isVisible: config,
+                });
+            };
+
+            const config = [];
+            data.columns.map((col, idx) => {
+                if (idx === 0) return;
+                const fieldName = col[0];
+                const color = data.colors[fieldName];
+                const name = data.names[fieldName];
+
+                createCheckbox(name, idx, config, changeData, checkboxes, color);
+            });
+        }
+
+        switchData(data, renderChart.bind(shortChart));
+    };
 
     onChangeTheme = ({target}) => {
         const { checked } = target;
@@ -149,103 +181,6 @@ class Init {
         //init draggable slider
         new Slider(configSlider, data);
     }
-
-    init(id, data) {
-        const { id1, id2, shortChart, longChart } = this;
-
-        const {
-            wrap,
-            lineChart,
-            sliderElem,
-            dateElem,
-            columnsElem,
-            tooltipElem,
-            checkboxes,
-            labelText,
-            switcher,
-            wrapBlock,
-        } = this.template;
-
-        const { renderChart } = this;
-
-        const theme = {
-            day: {
-                wrap: 'transparent',
-                sliderElem: 'slider',
-                labelText: 'Switch to Night Mode',
-                tooltip: 'tooltip',
-                checkboxes: '#000',
-                mainColor: '#000',
-                subColor: '#fff',
-            },
-            night: {
-                wrap: '#242f3e',
-                sliderElem: 'slider-nightTheme',
-                labelText: 'Switch to Day Mode',
-                tooltip: 'tooltip-nightTheme',
-                checkboxes: '#fff',
-                mainColor: '#fff',
-                subColor: '#222f3f',
-            }
-        };
-
-        // switcher.addEventListener('change', onChange);
-        // switchTheme.call(this, 'day');
-        function onChange({target}) {
-            // const { checked } = target;
-            // const theme = checked ? 'night' : 'day';
-            // switchTheme(theme);
-        }
-        function switchTheme(mode) {
-            let newTheme = theme[mode];
-            let nightTheme = theme.night;
-
-            wrap.style.backgroundColor = newTheme.wrap;
-            labelText.innerText = newTheme.labelText;
-            checkboxes.style.color = newTheme.checkboxes;
-
-            if (mode === 'night') {
-                sliderElem.classList.add(nightTheme.sliderElem);
-                tooltipElem.classList.add(nightTheme.tooltip);
-
-            } else if (mode === 'day') {
-                sliderElem.classList.remove(nightTheme.sliderElem);
-                tooltipElem.classList.remove(nightTheme.tooltip);
-
-            }
-
-            renderChart.call(shortChart, {
-                theme: theme[mode],
-            })
-        }
-
-        function switchData(data) {
-            const changeData = (idx, color, e)=> {
-                const { checked } = e.target;
-                const parent = e.target.parentNode;
-                const customCheckbox = parent.querySelector('.custom-checkbox');
-
-                config[idx].isVisible = checked;
-                customCheckbox.style.backgroundColor = checked ? color : 'transparent';
-                shortChart.renderChart({
-                    isVisible: config,
-                });
-            };
-
-            const config = [];
-            data.columns.map((col, idx) => {
-                if (idx === 0) return;
-                const fieldName = col[0];
-                const color = data.colors[fieldName];
-                const name = data.names[fieldName];
-
-                createCheckbox(name, idx, config, changeData, checkboxes, color);
-            });
-        }
-
-        switchData(data, renderChart.bind(shortChart));
-    }
-
 }
 
 new Init(1, chart_data[0]);
